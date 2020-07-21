a0118e93fa98b9faea0c8e1e01bfb196893776fc
Callum Macrae <callum@macr.ae>
2020-07-21 18:28:16 +0100

    Draw images on pages!

--- src/components/book.ts
+++ src/components/book.ts
@@ -1,16 +1,22 @@
 import * as THREE from 'three';
-import { PhotoDescription} from '../utils/photo-data';
+import { PhotoDescription } from '../utils/photo-data';
 
-interface Page {
-  mesh: THREE.Mesh;
+interface Sheet {
+  object: THREE.Object3D;
   index: number;
 }
+
+// TERMINOLOGY:
+// there are one or more photos per page
+// there are two pages per sheet
+// the book is comprised of sheets + covers
+// the user looks at spreads, which is two pages, one from each sheet
 
 export default class Book {
   private group: THREE.Group;
   private frontCover: THREE.Object3D;
   private backCover: THREE.Object3D;
-  private sheets: Page[];
+  private sheets: Sheet[];
   private photos: PhotoDescription[];
   private currentSpread: number;
 
@@ -19,6 +25,7 @@ export default class Book {
     this.group = group;
 
     this.photos = photos;
+    // @TODO fix maths for this
     this.currentSpread = 4;
 
     const coverGeometry = new THREE.BoxGeometry(2.2, 3.2, 0.03);
@@ -36,20 +43,42 @@ export default class Book {
     this.backCover = backCover;
 
     const pageGeometry = new THREE.PlaneGeometry(2.1, 2.97);
-    pageGeometry.applyMatrix4(new THREE.Matrix4().makeTranslation(1.05, 0, 0));
-    const pageMaterial = new THREE.MeshStandardMaterial({
-      color: 0xffffff,
+    const plainMaterial = new THREE.MeshStandardMaterial({
+      color: 'white',
       side: THREE.DoubleSide
     });
 
     // A sheet is two pages (plus a blank one at beginning and end)
     const sheetCount = Math.ceil(photos.length / 2) + 2;
 
-    const sheets: Page[] = [];
+    const sheets: Sheet[] = [];
     for (let i = 0; i < sheetCount; i++) {
-      const sheet = new THREE.Mesh(pageGeometry, pageMaterial);
+      if (i === 0 || i === sheetCount - 1) {
+        const sheet = new THREE.Mesh(pageGeometry, plainMaterial);
+        group.add(sheet);
+        sheets.push({ object: sheet, index: i });
+        continue;
+      }
+
+      const sheet = new THREE.Group();
+
+      const frontPageMaterial = new THREE.MeshStandardMaterial({
+        map: this.getPageTexture(i * 2 - 2)
+      });
+
+      const frontPage = new THREE.Mesh(pageGeometry, frontPageMaterial);
+      sheet.add(frontPage);
+
+      const backPageMaterial = new THREE.MeshStandardMaterial({
+        map: this.getPageTexture(i * 2 - 1)
+      });
+
+      const backPage = new THREE.Mesh(pageGeometry, backPageMaterial);
+      backPage.rotateY(Math.PI);
+      sheet.add(backPage);
+
       group.add(sheet);
-      sheets.push({ mesh: sheet, index: i });
+      sheets.push({ object: sheet, index: i });
     }
     this.sheets = sheets;
 
@@ -72,7 +101,8 @@ export default class Book {
     this.backCover.rotation.y = this.getSheetAngle(this.sheets.length - 1);
 
     this.sheets.forEach((page, i) => {
-      page.mesh.rotation.y = this.getSheetAngle(i);
+      page.object.rotateY(this.getSheetAngle(i));
+      page.object.translateX(1.05);
     });
   }
 
@@ -88,5 +118,30 @@ export default class Book {
 
     const reverseIndex = this.sheets.length - 1 - index;
     return max - eachSheetAngle * reverseIndex;
+  }
+
+  private getPageTexture(pageIndex: number) {
+    const ctx = document.createElement('canvas').getContext('2d');
+
+    if (!ctx) {
+      // Dammit typescript
+      throw new Error('?!?!?!');
+    }
+
+    ctx.canvas.width = 210;
+    ctx.canvas.height = 297;
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 210, 297);
+
+    const photo = this.photos[pageIndex];
+
+    if (!photo || !photo.image) {
+      throw new Error('No image');
+    }
+
+    ctx.drawImage(photo.image, 50, 50, 100, 100);
+
+    return new THREE.CanvasTexture(ctx.canvas);
   }
 }
