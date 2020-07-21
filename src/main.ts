427ba0777b3d2860dcb3dc32a50905ff3921b9f2
Callum Macrae <callum@macr.ae>
2020-07-21 10:34:54 +0100

    Skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false;
+import './styles.css';
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+createApp(App)
+  .use(router)
+  .mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,16 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter);
-
-const routes: Array<RouteConfig> = [
+const routes: RouteRecordRaw[] = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
